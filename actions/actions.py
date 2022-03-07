b4cbd4db29897c38dc72ec3a990ed58f72f9fda2
HeerP <h4hero2004@yahoo.com>
2022-03-08 04:21:43 +0530

    Changed config to detect entities better, removed fuzzywuzzy due to public interest, still confused about wtf to do w coin symbols and names

--- actions/actions.py
+++ actions/actions.py
@@ -12,35 +12,104 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from datetime import datetime
+from rasa_sdk.events import SlotSet
 import sqlite3
 from ruamel.yaml import YAML
 from thefuzz import fuzz, process
+from ruamel.yaml.comments import CommentedMap as OrderedDict
 
 ### COIN UPDATE
 con = sqlite3.connect("database.db")
 cur = con.cursor()
-coin_ids = [a[0].replace('-', ' ') for a in cur.execute("SELECT id FROM coin_list")]
+all_coins = [a for a in cur.execute("SELECT id, symbol, name FROM coin_list")]
+coin_ids = []
+coin_symbol = []
+coin_names = []
+for i in all_coins:
+    coin_ids.append(i[0].replace('-', ' '))
+    coin_symbol.append(i[1])
+    coin_names.append(i[2])
 currencies = [a[0] for a in cur.execute("SELECT id FROM supported_currencies")]
 
 yaml = YAML()
-stream = open("./data/coins.yml", "r")
-stream1 = open("./data/currencies.yml", "r")
+stream = open("./data/coins.yml", "r", encoding = 'utf-8')
+stream1 = open("./data/currencies.yml", "r",encoding = 'utf-8')
+# stream2 = open("./data/coin_syn.yml", "r", encoding = 'utf-8')
 ste = yaml.load(stream)
 ste1 = yaml.load(stream1)
+# ste2 = yaml.load(stream2)
 stream.close()
 stream1.close()
+# stream2.close()
 
-stream = open("./data/coins.yml", "w")
-stream1 = open("./data/currencies.yml", "w")
+stream = open("./data/coins.yml", "w", encoding = 'utf-8')
+stream1 = open("./data/currencies.yml", "w", encoding = 'utf-8')
+# stream2 = open("./data/coin_syn.yml", "w", encoding = 'utf-8')
 
-ste["nlu"][0]['examples'] = '- ' + '\n- '.join(coin_ids)
+ste["nlu"][0]['examples'] = '- ' + '\n- '.join(coin_ids + coin_names)
 ste1["nlu"][0]['examples'] = '- ' + '\n- '.join(currencies)
+# num = 0
+# ste2['nlu'] = []
+# for i, j, k in zip(coin_ids, coin_symbol, coin_names):
+#     num = num + 1
+#     d = OrderedDict([('synonym', i),
+#         ('examples', f'- {j}\n- {k}')])
+#     ste2['nlu'].append(d)
+
+# print(num)
 
 yaml.dump(ste, stream)
 yaml.dump(ste1, stream1)
+# yaml.representer.ignore_aliases = lambda *data: True
+# yaml.dump(ste2, stream2)
 
 stream.close()
 stream1.close()
+# stream2.close()
+
+# def match_fuzzwuzz(coins, currency = None):
+#     matched_coins = []
+#     matched_currencies = []
+#     coins = [i.lower() for i in coins]
+#     currency = [i.lower() for i in currency]
+
+#     # FOR COINS
+#     if coins:
+#         if len(coins) == 1:
+#             if (coins[0]) not in coin_ids:
+#                 similar = process.extract(coins[0], coin_ids, scorer = fuzz.WRatio, limit = 5)
+#                 for i in similar:
+#                     matched_coins.append(i[0].replace(' ', '-'))
+#             else:
+#                 matched_coins = coins
+#         else:
+#             for i in coins:
+#                 if i not in coin_ids:
+#                     c = process.extractOne(i, coin_ids, scorer = fuzz.WRatio)[0]
+#                     matched_coins.append(c.replace(' ', '-'))
+#                 else:
+#                     matched_coins.append(i)
+    
+#     # FOR CURRENCIES
+#     if currency:
+#         if len(currency) == 1:
+#             if currency[0] not in currencies:
+#                 similar = process.extract(currency[0], currencies, scorer = fuzz.WRatio, limit = 2)
+#                 for i in similar:
+#                     matched_currencies.append(i[0])
+#             else:
+#                 matched_currencies = currency
+#         else:
+#             for i in currency:
+#                 if i not in currencies:
+#                     c = process.extractOne(i, currencies, scorer = fuzz.WRatio)[0]
+#                     matched_currencies.append(c)
+#                 else:
+#                     matched_currencies.append(i)
+                    
+#     return {"coins" : matched_coins, "currencies" : matched_currencies}
+
+        
 
 
 class ActionSearchCoin(Action):
@@ -54,27 +123,27 @@ class ActionSearchCoin(Action):
         msg = tracker.get_slot('coin')[0].lower()
         print(msg)
         ### If id directly available
-        if msg in coin_ids:
-            choices = [msg]
-        else:
-            dispatcher.utter_message("We could not find an exact match. Please check the ones below.")
-            similar = process.extract(msg, coin_ids, limit = 5, scorer = fuzz.WRatio)
-            choices = []
-            for match in similar:
-                choices.append(match[0])
-
-        print(choices)
+        # if msg in coin_ids:
+        #     choices = [msg.replace(' ', '-')]
+        # else:
+        #     dispatcher.utter_message("We could not find an exact match. Please check the ones below.")
+        #     similar = process.extract(msg, coin_ids, limit = 5, scorer = fuzz.WRatio)
+        #     choices = []
+        #     for match in similar:
+        #         choices.append(match[0].replace(' ', '-'))
+        # choices = match_fuzzwuzz(msg)['coins']
+        # print(choices)
         msg = {
             "intent": {
                 "name": tracker.get_intent_of_latest_message()
             },
             "slots": {
-                "coins": choices
+                "coins": msg
             },
             "timestamp": datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
         }
         dispatcher.utter_message(json_message=msg)
-        return []
+        return [SlotSet("coin", None)]
     
 class ActionFetchPrice(Action):
     def name(self) -> Text:
@@ -83,11 +152,15 @@ class ActionFetchPrice(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-            
-            coins = tracker.get_slot("coin")
-            currencies = tracker.get_slot("currency")
-            intent = tracker.get_intent_of_latest_message()
-            msg = {
+        coins = list(set(tracker.get_slot("coin")))
+        print(coins)
+        currencies = list(set(tracker.get_slot("currency")))
+        print(currencies)
+            # choices = match_fuzzwuzz(coins, currencies)
+            # coins = choices['coins']
+            # currencies = choices['currencies']
+        intent = tracker.get_intent_of_latest_message()
+        msg = {
             "intent": {
                 "name": intent
             },
@@ -96,5 +169,6 @@ class ActionFetchPrice(Action):
                 "currencies": currencies
             },
             "timestamp": datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-            }
-            dispatcher.utter_message(json_message=msg)
+        }
+        dispatcher.utter_message(json_message=msg)
+        return [SlotSet("coin", None), SlotSet("currency", None)]
--- test.py
+++ test.py
@@ -1,9 +1,36 @@
 import sqlite3
+from ruamel.yaml import YAML
+from collections import OrderedDict
+from ruamel.yaml.comments import CommentedMap as OrderedDict
+import sys
 
-
+yaml = YAML()
 con = sqlite3.connect("database.db")
 cur = con.cursor()
-alla = [a for a in cur.execute("SELECT symbol, name FROM coin_list WHERE id = \"bitcoin\"")][0]
-coin_s, coin_n = alla
-print(coin_s, coin_n)
+alla = [a for a in cur.execute("SELECT id, symbol, name FROM coin_list")]
+coin_ids = []
+coin_symbol = []
+coin_names = []
+for i in alla:
+    coin_ids.append(i[0].replace('-', ' '))
+    coin_symbol.append(i[1])
+    coin_names.append(i[2])
+
+# for i, j, k in zip(coin_ids, coin_symbol, coin_names):
+#     print(i, j, k)
+
+file_coins = open('coins.yml', "r")
+file_coinsym = open('coin_syn.yml', "r", encoding = 'utf-8')
+stream_coins = yaml.load(file_coins)
+stream_coinsym = yaml.load(file_coinsym)
+print(stream_coinsym)
+for i, j, k in zip(coin_ids, coin_symbol, coin_names):
+    d = OrderedDict([('synonym', i),
+        ('examples', [j, k])])
+    stream_coinsym['nlu'].append(d)
+yaml.representer.ignore_aliases = lambda *data: True
+file_coins.close()
+file_coinsym.close()
+file_coinsym_write = open('coin_syn.yml', 'w', encoding='utf-8')
+yaml.dump(stream_coinsym, file_coinsym_write)
 cur.close()
